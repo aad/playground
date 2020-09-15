139b3c7e95cc1d44d437b02a6da8b9923dafcd24
Samson Yu <Samson.yu@fil.com>
2020-09-15 11:48:42 +0800

    contextmanager and python 3 new features

---
+++ python/contextmanager/start.sh
@@ -0,0 +1,4 @@
+pipenv run jupyter contrib nbextension install 
+pipenv run jupyter nbextensions_configurator enable
+pipenv run jupyter nbextension enable splitcell/splitcell
+pipenv run jupyter notebook
